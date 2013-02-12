813e20abf4e8bab7ea21135dd7ced6d2e90eb7e9
Joeboy <joe.button@aviva.co.uk>
2013-02-12 09:45:27 +0000

    Simplify ringbuffer

--- pitracker/includes/pi/audio.c
+++ pitracker/includes/pi/audio.c
@@ -7,34 +7,29 @@
 extern void PUT32 (uint32_t,uint32_t);
 extern uint32_t GET32 (uint32_t);
 
-volatile uint32_t* gpio = (void*)GPIO_BASE;
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *((uint32_t*)GPIO_BASE+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
 
 typedef struct {
     uint32_t buffer_sz; // in uint32_ts
     uint32_t *buffer;
     uint32_t read_p, write_p;
-    uint32_t dmas_per_buffer;
-    uint32_t dma_sz;    // in uint32_ts
     void *dma_cb_container;
-    struct bcm2708_dma_cb *dma_cb_chain;
+    struct bcm2708_dma_cb *dma_cb;
 } ringbuffer_t;
 
 static ringbuffer_t *buf;
 
-ringbuffer_t * new_ringbuffer(uint32_t buffer_sz, uint32_t dmas_per_buffer) {
+ringbuffer_t * new_ringbuffer(uint32_t buffer_sz) {
     ringbuffer_t *rb = malloc(sizeof(ringbuffer_t));
     rb->buffer_sz = buffer_sz;
     rb->buffer = malloc(buffer_sz * sizeof(uint32_t));
-    rb->dmas_per_buffer = dmas_per_buffer;
-    rb->dma_sz = (float)buffer_sz / dmas_per_buffer;// Vile hack - TODO: fix
     rb->read_p = 0;
     rb->write_p = 0;
-    for (uint32_t i=0;i<buffer_sz;i++) rb->buffer[i]=512;
-    rb->dma_cb_container = malloc(0x20 + sizeof(struct bcm2708_dma_cb) * rb->dmas_per_buffer);
+    for (uint32_t i=0;i<buffer_sz;i++) rb->buffer[i] = 512;
+    rb->dma_cb_container = malloc(0x20 + sizeof(struct bcm2708_dma_cb));
     uint32_t offset = (uint32_t)rb->dma_cb_container & 0x1f;
-    rb->dma_cb_chain = rb->dma_cb_container + 0x20 - offset;
+    rb->dma_cb = rb->dma_cb_container + 0x20 - offset;
     return rb;
 }
 
@@ -46,7 +41,7 @@ void delete_ringbuffer(ringbuffer_t *rb) {
 
 int32_t audio_buffer_free_space() {
     // Return the number of uint32_ts that can be safely written to the buffer
-    buf->read_p = buf->dma_sz * ((struct bcm2708_dma_cb*)GET32(DMA5_CNTL_BASE + BCM2708_DMA_ADDR) - buf->dma_cb_chain);
+    buf->read_p = ((uint32_t*)GET32(DMA5_CNTL_BASE + 0x0c) - buf->buffer);
     uint32_t spare;
     if (buf->read_p >= buf->write_p) {
         spare = buf->read_p - buf->write_p;
@@ -68,7 +63,7 @@ void audio_buffer_write(float *audio_buf_left, float *audio_buf_right, size_t si
 
 
 int32_t audio_init(void) {
-    buf = new_ringbuffer(256, 16); // 256 byte ringbuffer, split over 16 dma windows
+    buf = new_ringbuffer(256);
     // Set up the pwm clock
     // vals read from raspbian:
     // PWMCLK_CNTL = 148 = 10010100 - 100 is allegedly 'plla' but I can't make that work
@@ -100,15 +95,11 @@ int32_t audio_init(void) {
     PUT32(PWM_BASE + 4*BCM2835_PWM1_RANGE, range);
     usleep(1);
 
-    uint32_t i;
-    for (i=0;i<buf->dmas_per_buffer;i++) {
-        buf->dma_cb_chain[i].info   = BCM2708_DMA_S_INC | BCM2708_DMA_WAIT_RESP | BCM2708_DMA_D_DREQ | BCM2708_DMA_PER_MAP(5);
-        buf->dma_cb_chain[i].src = (uint32_t)(buf->buffer + i * buf->dma_sz);
-        buf->dma_cb_chain[i].dst = ((PWM_BASE+4*BCM2835_PWM_FIFO) & 0x00ffffff) | 0x7e000000; // physical address of fifo
-        buf->dma_cb_chain[i].length = sizeof(uint32_t) * buf->dma_sz;
-        buf->dma_cb_chain[i].next = (uint32_t)&buf->dma_cb_chain[i+1];
-    }
-    buf->dma_cb_chain[i-1].next = (uint32_t)&buf->dma_cb_chain[0];
+    buf->dma_cb->info   = BCM2708_DMA_S_INC | BCM2708_DMA_WAIT_RESP | BCM2708_DMA_D_DREQ | BCM2708_DMA_PER_MAP(5);
+    buf->dma_cb->src = (uint32_t)(buf->buffer);
+    buf->dma_cb->dst = ((PWM_BASE+4*BCM2835_PWM_FIFO) & 0x00ffffff) | 0x7e000000; // physical address of fifo
+    buf->dma_cb->length = sizeof(uint32_t) * buf->buffer_sz;
+    buf->dma_cb->next = (uint32_t)buf->dma_cb;
     usleep(1);
     PUT32(PWM_BASE + 4*BCM2835_PWM_DMAC, PWMDMAC_ENAB | PWMDMAC_THRSHLD);
     usleep(1);
@@ -127,7 +118,7 @@ int32_t audio_init(void) {
     PUT32(DMA5_CNTL_BASE + BCM2708_DMA_CS, BCM2708_DMA_RESET);
     usleep(10);
     PUT32(DMA5_CNTL_BASE + BCM2708_DMA_CS, BCM2708_DMA_INT | BCM2708_DMA_END);
-    PUT32(DMA5_CNTL_BASE + BCM2708_DMA_ADDR, (uint32_t)&buf->dma_cb_chain[0]);
+    PUT32(DMA5_CNTL_BASE + BCM2708_DMA_ADDR, (uint32_t)buf->dma_cb);
     PUT32(DMA5_CNTL_BASE + BCM2708_DMA_DEBUG, 7); // clear debug error flags
     usleep(10);
     PUT32(DMA5_CNTL_BASE + BCM2708_DMA_CS, 0x10880001);  // go, mid priority, wait for outstanding writes
